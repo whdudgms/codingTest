fe7700ada9867c27f2943f10543f538538e323ce
whdudgms <whdudgms123@naver.com>
2024-01-31 22:25:08 +0900

    10807풀고, 더 나은 풀이 찾아서 올림

--- src/BackJoon/B10807.java
+++ src/BackJoon/B10807.java
@@ -1,7 +1,40 @@
 package BackJoon;
 
+import java.util.Scanner;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class B10807 {
-    public static void main(String[] args){
-        System.out.println("Hello world");
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());;
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int b = Integer.parseInt(br.readLine());
+
+        for(int j = 0; j < arr.length; j++){
+            if(b == arr[j]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
+
+// 이문제를 그냥 Scanner로 풀었지만 BufferReader를 사용하면 훨씬 시간단축이 된다. 이풀이를 보고 공부해서 내 풀이로 만들자.
