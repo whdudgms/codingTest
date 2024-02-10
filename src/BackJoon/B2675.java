9edf1d87d91ef05772362bc72a2d24051efe3c05
whdudgms <whdudgms123@naver.com>
2024-02-10 17:10:04 +0900

    2/10일자 커밋

--- src/BackJoon/B2675.java
+++ src/BackJoon/B2675.java
@@ -1,4 +1,25 @@
 package BackJoon;
 
+import java.util.Scanner;
+
 public class B2675 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        String str = "";
+        String result = "";
+
+        for (int i = 0; i < num; i++) {
+            count = sc.nextInt();
+            str = sc.next();
+            for(int k = 0; k < str.length(); k++){
+                for (int j = 0; j < count; j++){
+                    result += str.charAt(k);
+                }
+            }
+            System.out.println(result);
+            result ="";
+        }
+    }
 }
