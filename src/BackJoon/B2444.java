8aaa24d823ddf887c4f2511a315680201b68f0a3
whdudgms <whdudgms123@naver.com>
2024-02-11 22:16:45 +0900

    2/11일자 커밋 못푼 문제 있음

---
+++ src/BackJoon/B2444.java
@@ -0,0 +1,18 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class B2444 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i < 2*n-1; i++){
+            for(int k = 0 ; k < n - i; k++){
+
+
+            }
+
+        }
+
+    }
+}
