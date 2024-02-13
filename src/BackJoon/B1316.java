c9f7f63ec990d951a8ee058c4874e927b43e52c5
whdudgms <whdudgms123@naver.com>
2024-02-13 21:43:31 +0900

    2/12일자 백준 커밋 1316,25206 못 풀어서 블로그내용임 ㅠㅠ 다시 풀어야함

--- src/BackJoon/B1316.java
+++ src/BackJoon/B1316.java
@@ -3,19 +3,53 @@ package BackJoon;
 import java.util.Scanner;
 
 public class B1316 {
+
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
-        String str = "";
-        int result =0;
+        int count = 0;
+        int N = in.nextInt();
 
-        for (int i = 0; i < num; i++) {
-            str = sc.nextLine();
-            for (int j = 0; j < str.length(); j++) {
-
+        for (int i = 0; i < N; i++) {
+            if (check() == true) {
+                count++;
             }
         }
+        System.out.println(count);
+    }
 
+    public static boolean check() {
+        boolean[] check = new boolean[26];
+        int prev = 0;
+        String str = in.next();
+
+        for(int i = 0; i < str.length(); i++) {
+            int now = str.charAt(i);	// i 번째 문자 저장 (현재 문자)
+
+
+            // 앞선 문자와 i 번째 문자가 같지 않다면?
+            if (prev != now) {
+
+                // 해당 문자가 처음 나오는 경우 (false 인 경우)
+                if ( check[now - 'a'] == false ) {
+                    check[now - 'a'] = true;		// true 로 바꿔준다
+                    prev = now;					// 다음 턴을 위해 prev 도 바꿔준다
+                }
+
+                // 해당 문자가 이미 나온 적이 있는 경우 (그룹단어가 아니게 됨)
+                else {
+                    return false;	//함수 종료
+                }
+            }
+
+
+            // 앞선 문자와 i 번째 문자가 같다면? (연속된 문자)
+            // else 문은 없어도 됨
+            else {
+                continue;
+            }
+        }
+        return true;
     }
 }
---
+++ src/BackJoon/B25206.java
@@ -0,0 +1,62 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B25206 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        double sumNumScore = 0;
+        double sumNum =0;
+
+        for (int i = 0; i < 20; i++) {
+            st = new StringTokenizer(br.readLine());
+            String object = st.nextToken();
+            double num = Double.parseDouble(st.nextToken());
+            String alphabet = st.nextToken();
+            double score = 0 ;
+
+
+            if(!(alphabet.equals("P"))){
+
+                switch (alphabet) {
+                    case "A+":
+                        score = 4.5;
+                        break;
+                    case "A0":
+                        score = 4.0;
+                        break;
+                    case "B+":
+                        score = 3.5;
+                        break;
+                    case "B0":
+                        score = 3.0;
+                        break;
+                    case "C+":
+                        score = 2.5;
+                        break;
+                    case "C0":
+                        score = 2.0;
+                        break;
+                    case "D+":
+                        score = 1.5;
+                        break;
+                    case "F":
+                        score = 0.0;
+                        break;
+                    default:
+                        break;
+                }
+                sumNumScore += (num * score);
+                sumNum+= num;
+            }
+
+        }
+        System.out.printf("%.6f", sumNumScore / sumNum);
+        br.close();
+    }
+}
