5b7f4fc5bc39c393c4c61fcf0498ebbc8bd3d338
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-03-25 21:16:55 +0900

    색깔칠이기라고 생각하니까 쉽게 풀렸다

--- src/BackJoon/B2563.java
+++ src/BackJoon/B2563.java
@@ -5,29 +5,34 @@ import java.util.Scanner;
 public class B2563 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int papers = sc.nextInt();
-        int[][] point = new int[100][100];
+        int result = 0;
+        int stPoint1 = 0;
+        int stPoint2 = 0;
+        boolean[][] flag = new boolean[100][100];
 
-        int he;
-        int br;
-        for(int i =0; i < papers; i++){
-            he = sc.nextInt();
-            br = sc.nextInt();
+        int nums = sc.nextInt();
 
-            for(int j =0; j < 10; j++) {
-                for(int k =0; k < 10; j++) {
-                    point[he-1][br + k-1] = 1;
+        for (int i = 0 ; i < nums; i++){
+            stPoint1 = sc.nextInt() ;
+
+            stPoint2 = sc.nextInt() ;
+
+            for (int j = stPoint1; j < stPoint1+10; j++ ){
+                for (int k = stPoint2; k < stPoint2+10; k++ ){
+                    flag[j][k] = true;
                 }
-                he++;
             }
         }
-        int count =0;
-        for(int j =0; j < 100; j++) {
-            for(int k =0; k < 100; j++) {
-                if(point[j][k] ==1)
-                count++;
+
+        for (int j = 0; j < 100; j++ ){
+            for (int k = 0; k < 100; k++ ){
+                if(flag[j][k] == true){
+                    result++;
+                }
             }
         }
-        System.out.println(count);
+
+        System.out.print(result);
+
     }
 }
