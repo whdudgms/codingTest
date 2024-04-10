f6107aa826e11379fa417900761a1731392fb403
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-04-10 13:47:07 +0900

    4/10 TO DO BAEKJOON PROBLEM

---
+++ src/BackJoon/B15894.java
@@ -0,0 +1,17 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class B15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long num = sc.nextInt();
+        long result;
+        result = num * 4;
+
+        System.out.println(num * 4);
+
+
+    }
+}
